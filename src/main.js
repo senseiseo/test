29261a0d61a1f3c5a2aa381755adebe69e87c6c7
senseiseo <1919mur@gmail.com>
2021-06-09 22:11:28 +0300

    Add coment

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config'
 
+import Button from 'primevue/button'
+import Menubar from 'primevue/menubar';
+const app = createApp(App)
+
+app.use(PrimeVue)
+
+app.component('Button', Menubar)
+app.component('Button', Button)
+app.use(PrimeVue, {ripple: true});
+
+  
 createApp(App).mount('#app')
